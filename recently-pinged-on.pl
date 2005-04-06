7cf3f7fe72d04a5197b2ac265e0a0b8e27e19790
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2005-04-06 13:11:22 +0000

    Fix an embarassing bug.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-RecentlyPingedOn/trunk@9 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- recently-pinged-on.pl
+++ recently-pinged-on.pl
@@ -1,6 +1,6 @@
 # A plugin for adding 'recently_pinged_on' option to MTEntries container
 #
-# Release 0.15 (Feb 12, 2005)
+# Release 0.16 (Feb 19, 2005)
 #
 # This software is provided as-is. You may use it for commercial or 
 # personal use. If you distribute it, please keep this notice intact.
@@ -13,12 +13,13 @@ use vars qw($mt_hdlr_entries);
 if (MT->can('add_plugin')) {
     require MT::Plugin;
     my $plugin = new MT::Plugin();
-    $plugin->name("recently_pinged_on Plugin 0.15");
+    $plugin->name("recently_pinged_on Plugin 0.16");
     $plugin->description("Add 'recently_ping_on' option to MTEntries container");
     $plugin->doc_link("http://as-is.net/hacks/2005/01/recently_pinged_on_plugin.html");
     MT->add_plugin($plugin);
 }
 
+use MT::Template::Context;
 {
     local $SIG{__WARN__} = sub { };
     $mt_hdlr_entries = \&MT::Template::Context::_hdlr_entries;
